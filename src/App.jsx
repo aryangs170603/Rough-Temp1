a971bb15a433b3b8fcd9a3eecaed8379f36570f5
Anirudhh behera <anirudhhbehera@gmail.com>
2024-06-28 12:56:19 +0000

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
@@ -10,11 +10,14 @@ import StudentsReview from './components/StudentsReview.jsx';
 import Awards from './components/Awards.jsx';
 import Footer from './components/Footer.jsx';
 import Contact from './components/Contact';
+const AppContent = () => {
+  const location = useLocation();
+  const hide = location.pathname === '/contact';
 
-const App = () => (
-  <Router>
+  return (
     <div className="App">
-      <Header />
+
+      {!hide && <Header />}
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="Companies" element={<Companies />} />
@@ -24,10 +27,37 @@ const App = () => (
         <Route path="contact" element={<Contact />} />
         <Route path="awards" element={<Awards />} />
       </Routes>
-      <Footer />
-      
+      {!hide && <Companies />}
+      {!hide && <AboutUs />}
+      {!hide && <StudentsPlaced />}
+      {!hide && <Awards />}
+      {!hide && <Footer />}
     </div>
+  );
+};
+
+const App = () => (
+  <Router>
+    <AppContent />
   </Router>
 );
+
+
+// const App = () => (
+//   <Router>
+//     <div className="App">
+//       <Header />
+      
+//       <Companies></Companies>
+//       <AboutUs></AboutUs>
+//       <StudentsPlaced></StudentsPlaced>
+//       <StudentsReview></StudentsReview>
+//       {/* <Contact></Contact> */}
+//       <Awards></Awards>
+//       <Footer />
+      
+//     </div>
+//   </Router>
+// );
 
 export default App;
